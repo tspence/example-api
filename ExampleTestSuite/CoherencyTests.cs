8ce3be56de6f87a4e2e431435439af5930846fd8
Ted Spence <ted@spence.net>
2023-01-14 17:41:03 +1300

    Work in progress on api explorer test

--- ExampleTestSuite/CoherencyTests.cs
+++ ExampleTestSuite/CoherencyTests.cs
@@ -1,6 +1,13 @@
 using AutoMapper;
 using ExampleBusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace ExampleTestSuite
 {
@@ -18,7 +25,26 @@ namespace ExampleTestSuite
         [TestMethod]
         public void TestSwashbuckleDocumentation()
         {
-            // A test to make sure all models and entities are fully mapped to each other
+            // Fetch the OpenAPI specification for this site and assert that all methods and models
+            // have documentation.  We do this by scanning for controllers and parameters.
+            var assembly = typeof(ExampleApi.Program).Assembly;
+            var modelTypes = new List<Type>();
+            foreach (var cls in assembly.GetTypes())
+            {
+                if (cls.IsSubclassOf(typeof(ControllerBase)))
+                {
+                    Console.WriteLine(cls.FullName);
+
+                    // Iterate through all methods on this class and make sure they have necessary documentation
+                    // But only if they have an [HttpGet] or similar annotation on them
+                    foreach (var method in (from m in cls.GetMethods() where m.GetCustomAttributes<HttpMethodAttribute>().Any() select m))
+                    {
+                        Console.WriteLine(method.Name);
+                    }
+                }
+            }
+
+            // Check all data model types
         }
 
         [TestMethod]
